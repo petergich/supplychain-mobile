b1ae3cc6f0e17835de3f5477b587bb68b9da5cf1
petergich <petergich80@gmail.com>
2024-08-16 17:17:04 +0300

    api intergration

---
+++ lib/apis/apis.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+const domain = "http://192.168.254.32:8080";
+var products = [];
+
+class ProductsApis {
+  const ProductsApis();
+  Future<List<Map<String, dynamic>>> getAllProducts() async {
+    const url = domain + "/products/all";
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    if (response.statusCode == 200) {
+      final responseBody = json.decode(response.body);
+      print("fetch completed");
+      print(responseBody);
+
+      // Parse the JSON response
+      final List<dynamic> productsJson = responseBody['products'];
+      // Convert to List<Map<String, dynamic>>
+      final List<Map<String, dynamic>> products =
+          productsJson.map((json) => json as Map<String, dynamic>).toList();
+      print(products);
+      return products;
+    } else {
+      throw Exception("Error occured while trying to fetch");
+    }
+  }
+}
--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,10 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:test_app/apis/apis.dart';
 import 'package:test_app/app_drawer.dart';
 import 'package:test_app/app_header.dart';
+import 'package:test_app/products.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -11,12 +15,50 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: AppHeader(title: "home"),
-      drawer: AppDrawer(),
-      body: Center(
-        child: Text('Welcome to the Home page!'),
+    Future<List<Map<String, dynamic>>> fetchProducts() async {
+      var products = await ProductsApis().getAllProducts();
+      return products;
+    }
+
+    var products = fetchProducts();
+    return Scaffold(
+      appBar: const AppHeader(title: "home"),
+      drawer: const AppDrawer(),
+      body: Container(
+        margin: const EdgeInsets.all(20),
+        // height:MediaQuery.of(context).size.height*0.3,
+        child: const Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Last production:"+"--"),
+                Text("Date:"+"--"),
+                Text("Quantity:"+"--")
+
+          ]),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                Text("Last purchase:" + "--"),
+                Text("Date:" + "--"),
+                Text("Status:" + "--")
+              ]),
+
+              Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+              Text("product 1" + "--"),
+              Text("product 2" + "--"),
+              Text("product 3" + "--")
+            ]),
+          ],
+        ),
       ),
+      
     );
   }
 }
